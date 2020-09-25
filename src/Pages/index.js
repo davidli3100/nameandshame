424e8add2834ec26d878c31e271804897055ee30
David Li <david.li3100@gmail.com>
2020-09-25 14:11:37 -0400

    styling: add grid styling for cards

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,12 +1,32 @@
-import React, { Fragment } from 'react'
-import CompanyCard from '../Components/CompanyCard'
-import Hero from '../Components/Hero'
+import { Box } from "@chakra-ui/core";
+import React, { Fragment } from "react";
+import CompanyCard from "../Components/CompanyCard";
+import Hero from "../Components/Hero";
 
 const Index = () => (
   <Fragment>
-    <Hero/>
-    <CompanyCard id="123" name="The Coca Cola Company" imageURL="https://fontmeme.com/images/Coca-Cola-Logo.jpg" numReports={192} score={3.2}/>
+    <Hero />
+    <Box px="80px">
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fit, 320px)"
+        gridColumnGap="40px"
+        gridRowGap="40px"
+        width="100%"
+      >
+        {/* temporary for testing the grid */}
+        {[0, 0, 0, 0, 0, 0, 0, 0].map((foo) => (
+          <CompanyCard
+            id="123"
+            name="The Coca Cola Company"
+            imageURL="https://fontmeme.com/images/Coca-Cola-Logo.jpg"
+            numReports={192}
+            score={3.2}
+          />
+        ))}
+      </Box>
+    </Box>
   </Fragment>
-)
+);
 
-export default Index
+export default Index;
