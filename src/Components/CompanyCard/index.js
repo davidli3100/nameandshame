8d16ffdffc220e3d744571ba5ddcacef11027981
David Li <david.li3100@gmail.com>
2020-09-25 19:27:04 -0400

    feat: use react-router-dom link component in buttons

--- src/Components/CompanyCard/index.js
+++ src/Components/CompanyCard/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Box, Icon, Image, Text, Button, Tooltip } from "@chakra-ui/core";
+import { Link } from "react-router-dom";
 
 /**
  * Handler to redirect the user when clicking on the company card's buttons
  * @param {"report" | "view"} type type of action taken
  * @param {string} id ID of the company/employer
+ * @returns {string} redirect uri
  */
 const handleClick = (type, id) => {
   switch (type) {
     case "report":
-      window.location.href = `/report/${id}`;
-      break;
+      return `/report/${id}`;
     case "view":
-      window.location.href = `/employer/${id}`;
-      break;
+      return `/employer/${id}`;
     default:
-      break;
+      return `/`
   }
 };
 
@@ -55,7 +55,8 @@ const CompanyCard = ({ id, name, imageURL, numReports, score }) => (
     </Box>
     <Box mt="24px">
       <Button
-        onClick={() => handleClick("report", id)}
+        as={Link}
+        to={handleClick("report", id)}
         variantColor="primary"
         width="calc(50% - 8px)"
         mr="16px"
@@ -64,7 +65,8 @@ const CompanyCard = ({ id, name, imageURL, numReports, score }) => (
         Report
       </Button>
       <Button
-        onClick={() => handleClick("view", id)}
+        as={Link}
+        to={handleClick("view", id)}
         color="blueGray.600"
         width="calc(50% - 8px)"
         borderRadius="6px"
