dbf982de04739f69fd78a2c08b2120dc64e68862
David Li <david.li3100@gmail.com>
2020-09-25 19:22:05 -0400

    feat: add sidebar, misc. styling

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/core";
+import { Box, Text } from "@chakra-ui/core";
 import React, { Fragment } from "react";
 import CompanyCard from "../Components/CompanyCard";
 import Hero from "../Components/Hero";
@@ -6,24 +6,33 @@ import Hero from "../Components/Hero";
 const Index = () => (
   <Fragment>
     <Hero />
-    <Box px="80px" py="64px">
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(auto-fit, 320px)"
-        gridColumnGap="40px"
-        gridRowGap="40px"
-        width="100%"
-      >
-        {/* temporary for testing the grid */}
-        {[0, 0, 0, 0, 0, 0, 0, 0].map((foo) => (
-          <CompanyCard
-            id="123"
-            name="The Coca Cola Company"
-            imageURL="https://fontmeme.com/images/Coca-Cola-Logo.jpg"
-            numReports={192}
-            score={3.2}
-          />
-        ))}
+    <Box px="64px" py="56px">
+      <Text fontSize="32px" color="blueGray.900" fontWeight="bold" mb="32px">
+        Browse Employers
+      </Text>
+      <Box display="grid" gridTemplateColumns="300px 1fr" gridColumnGap="40px">
+        <Box backgroundColor="blue.100">
+          Search bar and stuff
+        </Box>
+        <Box
+          justifyContent="end"
+          display="grid"
+          gridTemplateColumns="repeat(auto-fit, 320px)"
+          gridColumnGap="40px"
+          gridRowGap="40px"
+          width="100%"
+        >
+          {/* temporary for testing the grid */}
+          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((foo) => (
+            <CompanyCard
+              id="123"
+              name="The Coca Cola Company"
+              imageURL="https://fontmeme.com/images/Coca-Cola-Logo.jpg"
+              numReports={192}
+              score={3.2}
+            />
+          ))}
+        </Box>
       </Box>
     </Box>
   </Fragment>
