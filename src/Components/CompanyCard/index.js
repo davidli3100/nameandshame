351a9da52e8fca29167fee3f044b5123d058ee60
David Li <david.li3100@gmail.com>
2020-09-25 13:08:59 -0400

    feat: company card design
    - add tooltips
    - add buttons
    - fix styling

--- src/Components/CompanyCard/index.js
+++ src/Components/CompanyCard/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Box, Icon, Image, Text } from "@chakra-ui/core";
+import { Box, Icon, Image, Text, Button, Tooltip } from "@chakra-ui/core";
 
 const CompanyCard = ({ id, name, imageURL, numReports, score }) => (
-  <Box>
+  <Box
+    width="100%"
+    maxWidth="320px"
+    px="20px"
+    py="20px"
+    borderColor="blueGray.100"
+    borderWidth="2px"
+    borderRadius="8px"
+  >
     <Image
       objectFit="cover"
       height="75px"
@@ -10,14 +18,30 @@ const CompanyCard = ({ id, name, imageURL, numReports, score }) => (
       borderRadius="8px"
       src={imageURL}
     />
-    <Text>{name}</Text>
-    <Box>
-      <Icon name="reports" color="blueGray.600" />
-      {numReports}
+    <Text pt="12px" fontSize="18px" color="blueGray.900" fontWeight="700">
+      {name}
+    </Text>
+    <Box display="flex" flexDirection="row" mt="6px" fontWeight="600">
+      <Tooltip label="Number of reports" placement="bottom">
+        <Box color="blueGray.600" mr="32px" display="flex" flexDirection="row">
+          <Icon mt="2px" mr="8px" size="20px" name="reports" />
+          <Text>{numReports}</Text>
+        </Box>
+      </Tooltip>
+      <Tooltip label="Toxicity Score (0-10)" placement="bottom">
+        <Box color="blueGray.600" display="flex" flexDirection="row">
+          <Icon mt="2px" size="20px" mr="8px" name="score" />
+          <Text>{score}</Text>
+        </Box>
+      </Tooltip>
     </Box>
-    <Box>
-      <Icon name="score" color="blueGray.600" />
-      {score}
+    <Box mt="24px">
+      <Button variantColor="primary" width="calc(50% - 8px)" mr="16px">
+        Report
+      </Button>
+      <Button color="blueGray.600" width="calc(50% - 8px)">
+        View
+      </Button>
     </Box>
   </Box>
 );
