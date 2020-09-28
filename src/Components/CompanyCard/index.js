3435b0b28693bbfc8a0fdda17c22f07753a3e425
David Li <david.li3100@gmail.com>
2020-09-28 16:48:29 -0400

    feat: connect algolia, add basic filters

--- src/Components/CompanyCard/index.js
+++ src/Components/CompanyCard/index.js
@@ -15,66 +15,78 @@ const handleClick = (type, id) => {
     case "view":
       return `/employer/${id}`;
     default:
-      return `/`
+      return `/`;
   }
 };
 
-const CompanyCard = ({ id, name, imageURL, numReports, score }) => (
-  <Box
-    width="100%"
-    maxWidth="320px"
-    px="20px"
-    py="20px"
-    borderColor="blueGray.100"
-    borderWidth="1px"
-    borderRadius="8px"
-  >
-    <Image
-      objectFit="cover"
-      height="75px"
-      width="75px"
+const CompanyCard = (props) => {
+  const { objectID: id, name, imageURL, numReports, score } = props.hit;
+  return (
+    <Box
+      width="100%"
+      maxWidth="320px"
+      px="20px"
+      py="20px"
+      borderColor="blueGray.100"
+      borderWidth="1px"
       borderRadius="8px"
-      src={imageURL}
-    />
-    <Text pt="12px" fontSize="18px" color="blueGray.900" fontWeight="700">
-      {name}
-    </Text>
-    <Box display="flex" flexDirection="row" mt="6px" fontWeight="600">
-      <Tooltip fontWeight="600" label="Number of reports" placement="bottom">
-        <Box color="blueGray.600" mr="32px" display="flex" flexDirection="row">
-          <Icon mt="2px" mr="8px" size="20px" name="reports" />
-          <Text>{numReports}</Text>
-        </Box>
-      </Tooltip>
-      <Tooltip fontWeight="600" label="Toxicity Score (0-10)" placement="bottom">
-        <Box color="blueGray.600" display="flex" flexDirection="row">
-          <Icon mt="2px" size="20px" mr="8px" name="score" />
-          <Text>{score}</Text>
-        </Box>
-      </Tooltip>
+    >
+      <Image
+        objectFit="cover"
+        height="75px"
+        width="75px"
+        borderRadius="8px"
+        src={imageURL}
+      />
+      <Text pt="12px" fontSize="18px" color="blueGray.900" fontWeight="700">
+        {name}
+      </Text>
+      <Box display="flex" flexDirection="row" mt="6px" fontWeight="600">
+        <Tooltip fontWeight="600" label="Number of reports" placement="bottom">
+          <Box
+            color="blueGray.600"
+            mr="32px"
+            display="flex"
+            flexDirection="row"
+          >
+            <Icon mt="2px" mr="8px" size="20px" name="reports" />
+            <Text>{numReports}</Text>
+          </Box>
+        </Tooltip>
+        <Tooltip
+          fontWeight="600"
+          label="Toxicity Score (0-10)"
+          placement="bottom"
+        >
+          <Box color="blueGray.600" display="flex" flexDirection="row">
+            <Icon mt="2px" size="20px" mr="8px" name="score" />
+            <Text>{score}</Text>
+          </Box>
+        </Tooltip>
+      </Box>
+      <Box mt="24px">
+        <Button
+          as={Link}
+          to={handleClick("report", id)}
+          variantColor="primary"
+          width="calc(50% - 8px)"
+          mr="16px"
+          borderRadius="6px"
+        >
+          Report
+        </Button>
+        <Button
+          as={Link}
+          to={handleClick("view", id)}
+          color="blueGray.600"
+          width="calc(50% - 8px)"
+          borderRadius="6px"
+        >
+          View
+        </Button>
+      </Box>
     </Box>
-    <Box mt="24px">
-      <Button
-        as={Link}
-        to={handleClick("report", id)}
-        variantColor="primary"
-        width="calc(50% - 8px)"
-        mr="16px"
-        borderRadius="6px"
-      >
-        Report
-      </Button>
-      <Button
-        as={Link}
-        to={handleClick("view", id)}
-        color="blueGray.600"
-        width="calc(50% - 8px)"
-        borderRadius="6px"
-      >
-        View
-      </Button>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default CompanyCard;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,7 +1,22 @@
 import { Box, Text } from "@chakra-ui/core";
 import React, { Fragment } from "react";
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, connectHits, SortBy, RefinementList, NumericMenu, Panel } from 'react-instantsearch-dom';
 import CompanyCard from "../Components/CompanyCard";
 import Hero from "../Components/Hero";
+
+const searchClient = algoliasearch(
+  'FEQR412FHW',
+  '1eeb94ee61382152f97c1f18b869926c'
+);
+
+const customEmployerHits = ({ hits }) => (
+  (hits.map(hit => (
+    <CompanyCard hit={hit} />
+  )))
+);
+
+const EmployerHits = connectHits(customEmployerHits);
 
 const Index = () => (
   <Fragment>
@@ -10,30 +25,44 @@ const Index = () => (
       <Text fontSize="32px" color="blueGray.900" fontWeight="bold" mb="32px">
         Browse Employers
       </Text>
-      <Box display="grid" gridTemplateColumns="300px 1fr" gridColumnGap="40px">
-        <Box backgroundColor="blue.100">
-          Search bar and stuff
-        </Box>
-        <Box
-          justifyContent="end"
-          display="grid"
-          gridTemplateColumns="repeat(auto-fit, 320px)"
-          gridColumnGap="40px"
-          gridRowGap="40px"
-          width="100%"
-        >
-          {/* temporary for testing the grid */}
-          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((foo) => (
-            <CompanyCard
-              id="123"
-              name="The Coca Cola Company"
-              imageURL="https://fontmeme.com/images/Coca-Cola-Logo.jpg"
-              numReports={192}
-              score={3.2}
+      <InstantSearch indexName="Employers" searchClient={searchClient}>
+        <Box display="grid" gridTemplateColumns="300px 1fr" gridColumnGap="40px">
+          <Box>
+            <SearchBox />
+            <SortBy
+              defaultRefinement="Employers"
+              items={[
+                { value: 'Employers', label: "Default" },
+                { value: "Employers_name_asc", label: "Name (A-Z)" },
+                { value: "Employers_name_desc", label: "Name (Z-A)" },
+                { value: "Employers_score_asc", label: "Score (asc)" },
+                { value: "Employers_score_desc", label: "Score (desc)" }
+              ]}
             />
-          ))}
+            <Panel header="Employees">
+              <NumericMenu
+                attribute="numEmployees"
+                items={[
+                  { label : '1 - 10', start:1, end: 10 },
+                  { label: '10 - 100', start: 10, end: 100 },
+                  { label: '100 - 1000', start: 100, end: 1000 },
+                  { label: '1000 - 5000', start: 1000, end: 5000 },
+                  { label: '10000+', start: 10000 }
+                ]}
+              />
+            </Panel>
+          </Box>
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(auto-fit, 320px)"
+            gridColumnGap="40px"
+            gridRowGap="40px"
+            maxWidth="100%"
+          >
+            <EmployerHits />
+          </Box>
         </Box>
-      </Box>
+      </InstantSearch>
     </Box>
   </Fragment>
 );
