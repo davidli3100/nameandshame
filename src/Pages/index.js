b27844ec0f7b8d36f3c312e755493f8019ae6d35
David Li <david.li3100@gmail.com>
2020-09-30 10:16:14 -0400

    styling: responsive grid, remove border

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,22 +1,24 @@
 import { Box, Text } from "@chakra-ui/core";
 import React, { Fragment } from "react";
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, connectHits, SortBy, Panel } from 'react-instantsearch-dom';
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  connectHits,
+  SortBy,
+  Panel,
+} from "react-instantsearch-dom";
 import CompanyCard from "../Components/CompanyCard";
 import Hero from "../Components/Hero";
 import CustomRangeSlider from "../Components/RangeSlider";
 import CustomSearch from "../Components/CustomSearch";
 
 const searchClient = algoliasearch(
-  'FEQR412FHW',
-  '1eeb94ee61382152f97c1f18b869926c'
+  "FEQR412FHW",
+  "1eeb94ee61382152f97c1f18b869926c"
 );
 
-const customEmployerHits = ({ hits }) => (
-  (hits.map(hit => (
-    <CompanyCard hit={hit} />
-  )))
-);
+const customEmployerHits = ({ hits }) =>
+  hits.map((hit) => <CompanyCard hit={hit} />);
 
 const EmployerHits = connectHits(customEmployerHits);
 
@@ -28,22 +30,32 @@ const Index = () => (
         Browse Employers
       </Text>
       <InstantSearch indexName="Employers" searchClient={searchClient}>
-        <Box display="grid" gridTemplateColumns="270px 1fr" gridColumnGap="60px">
+        <Box
+          display="grid"
+          gridTemplateColumns={["1fr", null, null, null, "270px 1fr"]}
+          gridColumnGap="80px"
+        >
           <Box>
             <CustomSearch />
             <Box mt="24px" mb="32px">
               <Panel header="Sort">
-                <Box mt="16px">
+                <Box mt="8px">
                   <SortBy
                     defaultRefinement="Employers"
                     items={[
-                      { value: 'Employers', label: "Default" },
+                      { value: "Employers", label: "Default" },
                       { value: "Employers_name_asc", label: "Name (A-Z)" },
                       { value: "Employers_name_desc", label: "Name (Z-A)" },
                       { value: "Employers_score_asc", label: "Score (asc)" },
                       { value: "Employers_score_desc", label: "Score (desc)" },
-                      { value: "Employers_numReports_asc", label: "Reports (asc)" },
-                      { value: "Employers_numReports_desc", label: "Reports (desc)" }
+                      {
+                        value: "Employers_numReports_asc",
+                        label: "Reports (asc)",
+                      },
+                      {
+                        value: "Employers_numReports_desc",
+                        label: "Reports (desc)",
+                      },
                     ]}
                   />
                 </Box>
