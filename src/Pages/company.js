ac73d47d244f2f12a6ce927ef469b444071fcc4d
David Li <david.li3100@gmail.com>
2020-10-07 14:20:21 -0400

    fix: refactor async effect to avoid race condition

--- src/Pages/company.js
+++ src/Pages/company.js
@@ -8,35 +8,38 @@ import Stats from "../Components/Employers/Stats";
 import Reports from "../Components/Employers/Reports";
 
 const Company = () => {
-    const { id } = useParams();
-    const [employerData, setEmployerData] = useState({});
+  const { id } = useParams();
+  const [employerData, setEmployerData] = useState({});
 
-    useEffect(async () => {
-        // fetch data from firebase
-        const firebaseDoc = await firebase
-            .firestore()
-            .collection("employers")
-            .doc(id)
-            .get();
-        const data = firebaseDoc.data();
-        setEmployerData(data);
-        console.log(data);
-    }, []);
+  useEffect(() => {
+    // fetch data from firebase
+    const fetchFirebaseData = async () => {
+      const firebaseDoc = await firebase
+        .firestore()
+        .collection("employers")
+        .doc(id)
+        .get();
+      const data = firebaseDoc.data();
+      setEmployerData(data);
+      console.log(data);
+    };
+    fetchFirebaseData();
+  }, []);
 
-    return (
-        <Box display="flex" flexDirection="column" justifyContent="center">
-            <Banner source={employerData.backgroundImageURL} />
-            <Stats
-                logo={employerData.imageURL}
-                name={employerData.name}
-                employees={employerData.numEmployees}
-                reports={employerData.numReports}
-                trend={84}
-                mcr="Racism"
-            />
-            <Reports />
-        </Box>
-    );
+  return (
+    <Box display="flex" flexDirection="column" justifyContent="center">
+      <Banner source={employerData.backgroundImageURL} />
+      <Stats
+        logo={employerData.imageURL}
+        name={employerData.name}
+        employees={employerData.numEmployees}
+        reports={employerData.numReports}
+        trend={84}
+        mcr="Racism"
+      />
+      <Reports />
+    </Box>
+  );
 };
 
 export default Company;
