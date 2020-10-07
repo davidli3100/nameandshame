4129edff1e5d53d21c0309b42262e96d73601d51
David Li <david.li3100@gmail.com>
2020-10-07 14:30:10 -0400

    fix: fix banner styling

--- src/Components/Employers/Banner/index.js
+++ src/Components/Employers/Banner/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import { Box, Image } from "@chakra-ui/core";
 
 const Banner = ({ source }) => (
-    <Box>
-        <Box
-            height="300px"
-            overflow="hidden"
-            display="flex"
-            alignItems="center"
-        >
-            <Image src={source} width="100%" />
-        </Box>
+  <Box>
+    <Box maxHeight="300px" overflow="hidden" display="flex" alignItems="center">
+      <Image src={source} width="100%" />
     </Box>
+  </Box>
 );
 
 export default Banner;
