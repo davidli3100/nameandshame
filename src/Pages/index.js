319606d81c199ac834379593df3e2bbf8ed8a051
David Li <david.li3100@gmail.com>
2020-10-07 13:56:22 -0400

    feat: add mobile collapse panel for filters

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,11 @@
-import { Box, Text } from "@chakra-ui/core";
+import {
+  AccordionHeader,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Text,
+} from "@chakra-ui/core";
 import React, { Fragment } from "react";
 import algoliasearch from "algoliasearch/lite";
 import {
@@ -23,6 +30,38 @@ const customEmployerHits = ({ hits }) =>
 
 const EmployerHits = connectHits(customEmployerHits);
 
+const FilterPanel = () => (
+  <>
+    <Box mt="24px" mb="32px">
+      <Panel header="Sort">
+        <Box mt="8px">
+          <SortBy
+            defaultRefinement="Employers"
+            items={[
+              { value: "Employers", label: "Default" },
+              { value: "Employers_name_asc", label: "Name (A-Z)" },
+              { value: "Employers_name_desc", label: "Name (Z-A)" },
+              { value: "Employers_score_asc", label: "Score (asc)" },
+              { value: "Employers_score_desc", label: "Score (desc)" },
+              {
+                value: "Employers_numReports_asc",
+                label: "Reports (asc)",
+              },
+              {
+                value: "Employers_numReports_desc",
+                label: "Reports (desc)",
+              },
+            ]}
+          />
+        </Box>
+      </Panel>
+    </Box>
+    <Panel header="Employees">
+      <CustomRangeSlider attribute="numEmployees" min={1} max={10000} />
+    </Panel>
+  </>
+);
+
 const Index = () => (
   <Fragment>
     <Hero />
@@ -35,37 +74,30 @@ const Index = () => (
           display="grid"
           gridTemplateColumns={["1fr", null, null, null, "270px 1fr"]}
           gridColumnGap="80px"
-          gridRowGap="64px"
+          gridRowGap="20px"
         >
-          <Box>
+          <Box justifySelf="center" width={isMobile() ? "320px" : "100%"}>
             <CustomSearch />
-            <Box mt="24px" mb="32px">
-              <Panel header="Sort">
-                <Box mt="8px">
-                  <SortBy
-                    defaultRefinement="Employers"
-                    items={[
-                      { value: "Employers", label: "Default" },
-                      { value: "Employers_name_asc", label: "Name (A-Z)" },
-                      { value: "Employers_name_desc", label: "Name (Z-A)" },
-                      { value: "Employers_score_asc", label: "Score (asc)" },
-                      { value: "Employers_score_desc", label: "Score (desc)" },
-                      {
-                        value: "Employers_numReports_asc",
-                        label: "Reports (asc)",
-                      },
-                      {
-                        value: "Employers_numReports_desc",
-                        label: "Reports (desc)",
-                      },
-                    ]}
-                  />
-                </Box>
-              </Panel>
-            </Box>
-            <Panel header="Employees">
-              <CustomRangeSlider attribute="numEmployees" min={1} max={10000} />
-            </Panel>
+            {isMobile() ? (
+              <AccordionItem border="none">
+                <AccordionHeader mt="16px" px="0">
+                  <Box
+                    flex="1"
+                    textAlign="left"
+                    color="blueGray.900"
+                    fontWeight="600"
+                  >
+                    Filter
+                  </Box>
+                  <AccordionIcon color="blueGray.900" />
+                </AccordionHeader>
+                <AccordionPanel>
+                  <FilterPanel />
+                </AccordionPanel>
+              </AccordionItem>
+            ) : (
+              <FilterPanel />
+            )}
           </Box>
           <Box
             display="grid"
