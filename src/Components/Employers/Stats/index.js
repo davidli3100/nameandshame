413e4465b5ec5578dc3260bc866ca02b1b030380
David Li <david.li3100@gmail.com>
2020-10-09 10:35:58 -0400

    styling: objectfit cover

--- src/Components/Employers/Stats/index.js
+++ src/Components/Employers/Stats/index.js
@@ -2,70 +2,70 @@ import React from "react";
 import { Box, Heading, Image } from "@chakra-ui/core";
 
 const Stats = ({ logo, name, employees, reports, RE, trend, mcr }) => (
-    <Box display="flex" flexDirection="column" alignItems="center" mt="-50px">
-        <Image src={logo} size="100px" rounded="md" />
-        <Heading color="black" size="lg" fontWeight="600" mt="30px">
-            {name}
+  <Box display="flex" flexDirection="column" alignItems="center" mt="-50px">
+    <Image src={logo} size="100px" rounded="md" objectFit="cover" />
+    <Heading color="black" size="lg" fontWeight="600" mt="30px">
+      {name}
+    </Heading>
+    <Box
+      display="grid"
+      gridTemplateColumns="repeat(auto-fit, 200px)"
+      gridAutoFlow="row"
+      gridRowGap={5}
+      mt="50px"
+      px="50px"
+      width="100%"
+      justifyContent="center"
+    >
+      <Box textAlign="left">
+        <Heading color="blueGray.600" size="sm" fontWeight="600">
+          EMPLOYEES
         </Heading>
-        <Box
-            display="grid"
-            gridTemplateColumns="repeat(auto-fit, 200px)"
-            gridAutoFlow="row"
-            gridRowGap={5}
-            mt="50px"
-            px="50px"
-            width="100%"
-            justifyContent="center"
+        <Heading size="sm" fontWeight="600">
+          {employees >= 10000 ? "10,000+" : employees}
+        </Heading>
+      </Box>
+      <Box textAlign="left">
+        <Heading color="blueGray.600" size="sm" fontWeight="600">
+          REPORTS
+        </Heading>
+        <Heading size="sm" fontWeight="600">
+          {reports}
+        </Heading>
+      </Box>
+
+      <Box textAlign="left">
+        <Heading color="blueGray.600" size="sm" fontWeight="600">
+          R/E
+        </Heading>
+        <Heading size="sm" fontWeight="600">
+          {(reports / employees).toFixed(3)}
+        </Heading>
+      </Box>
+      <Box textAlign="left">
+        <Heading color="blueGray.600" size="sm" fontWeight="600">
+          TREND (6M)
+        </Heading>
+        <Heading
+          size="sm"
+          fontWeight="600"
+          color={trend > 0 ? "red.500" : "blue.500"}
         >
-            <Box textAlign="left">
-                <Heading color="blueGray.600" size="sm" fontWeight="600">
-                    EMPLOYEES
-                </Heading>
-                <Heading size="sm" fontWeight="600">
-                    {employees >= 10000 ? "10,000+" : employees}
-                </Heading>
-            </Box>
-            <Box textAlign="left">
-                <Heading color="blueGray.600" size="sm" fontWeight="600">
-                    REPORTS
-                </Heading>
-                <Heading size="sm" fontWeight="600">
-                    {reports}
-                </Heading>
-            </Box>
+          {trend >= 0 ? "+" : "-"}
+          {trend}%
+        </Heading>
+      </Box>
 
-            <Box textAlign="left">
-                <Heading color="blueGray.600" size="sm" fontWeight="600">
-                    R/E
-                </Heading>
-                <Heading size="sm" fontWeight="600">
-                    {(reports / employees).toFixed(3)}
-                </Heading>
-            </Box>
-            <Box textAlign="left">
-                <Heading color="blueGray.600" size="sm" fontWeight="600">
-                    TREND (6M)
-                </Heading>
-                <Heading
-                    size="sm"
-                    fontWeight="600"
-                    color={trend > 0 ? "red.500" : "blue.500"}
-                >
-                    {trend >= 0 ? "+" : "-"}
-                    {trend}%
-                </Heading>
-            </Box>
-
-            <Box textAlign="left">
-                <Heading color="blueGray.600" size="sm" fontWeight="600">
-                    MCR
-                </Heading>
-                <Heading size="sm" fontWeight="600">
-                    {mcr}
-                </Heading>
-            </Box>
-        </Box>
+      <Box textAlign="left">
+        <Heading color="blueGray.600" size="sm" fontWeight="600">
+          MCR
+        </Heading>
+        <Heading size="sm" fontWeight="600">
+          {mcr}
+        </Heading>
+      </Box>
     </Box>
+  </Box>
 );
 
 export default Stats;
