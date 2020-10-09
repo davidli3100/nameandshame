16ab00dad0b61fb1e70060b0040304bfc1309593
David Li <david.li3100@gmail.com>
2020-10-09 09:12:51 -0400

    feat: pipe in algolia to employer

--- src/Components/Employers/Reports/index.js
+++ src/Components/Employers/Reports/index.js
@@ -3,40 +3,40 @@ import { Box } from "@chakra-ui/core";
 import Report from "../Report";
 
 const Reports = () => (
-    <Box
-        display="grid"
-        mt="50px"
-        py="50px"
-        px="100px"
-        gridTemplateColumns="repeat(auto-fit, minmax(400px, 600px))"
-        gridColumnGap="40px"
-        gridRowGap={5}
-        maxWidth="100%"
-        justifyContent="center"
-    >
-        <Report
-            title="Title of anonymous report being submitted to NameandShame"
-            date="Sept 20, 2020"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+  <Box
+    display="grid"
+    mt="50px"
+    py="50px"
+    px="100px"
+    gridTemplateColumns="repeat(auto-fit, minmax(400px, 600px))"
+    gridColumnGap="40px"
+    gridRowGap={5}
+    maxWidth="100%"
+    justifyContent="center"
+  >
+    <Report
+      title="Title of anonymous report being submitted to NameandShame"
+      date="Sept 20, 2020"
+      desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
             deserunt mollit anim id est laborum."
-        />
-        <Report
-            title="Title of anonymous report being submitted to NameandShame"
-            date="Sept 20, 2020"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    />
+    <Report
+      title="Title of anonymous report being submitted to NameandShame"
+      date="Sept 20, 2020"
+      desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
             deserunt mollit anim id est laborum."
-        />
-    </Box>
+    />
+  </Box>
 );
 
 export default Reports;
--- src/Pages/company.js
+++ src/Pages/company.js
@@ -5,41 +5,73 @@ import "firebase/firestore";
 import { useParams } from "react-router-dom";
 import Banner from "../Components/Employers/Banner";
 import Stats from "../Components/Employers/Stats";
-import Reports from "../Components/Employers/Reports";
+import {
+  connectHits,
+  InstantSearch,
+  connectRefinementList,
+} from "react-instantsearch-dom";
+import algoliasearch from "algoliasearch/lite";
+import Report from "../Components/Employers/Report";
+
+const searchClient = algoliasearch(
+  "FEQR412FHW",
+  "1eeb94ee61382152f97c1f18b869926c"
+);
+
+const customReportHits = ({ hits }) => hits.map((hit) => <Report hit={hit} />);
+
+const VirtualRefinementList = connectRefinementList(() => null);
+const EmployerHits = connectHits(customReportHits);
 
 const Company = () => {
-    const { id } = useParams();
-    const [employerData, setEmployerData] = useState({});
+  const { id } = useParams();
+  const [employerData, setEmployerData] = useState({});
 
-    useEffect(() => {
-        // fetch data from firebase
-        const fetchFirebaseData = async () => {
-            const firebaseDoc = await firebase
-                .firestore()
-                .collection("employers")
-                .doc(id)
-                .get();
-            const data = firebaseDoc.data();
-            setEmployerData(data);
-            console.log(data);
-        };
-        fetchFirebaseData();
-    }, [id]);
+  useEffect(() => {
+    // fetch data from firebase
+    const fetchFirebaseData = async () => {
+      const firebaseDoc = await firebase
+        .firestore()
+        .collection("employers")
+        .doc(id)
+        .get();
+      const data = firebaseDoc.data();
+      setEmployerData(data);
+      console.log(data);
+    };
+    fetchFirebaseData();
+  }, [id]);
 
-    return (
-        <Box display="flex" flexDirection="column" justifyContent="center">
-            <Banner source={employerData.backgroundImageURL} />
-            <Stats
-                logo={employerData.imageURL}
-                name={employerData.name}
-                employees={employerData.numEmployees}
-                reports={employerData.numReports}
-                trend={84}
-                mcr="Racism"
-            />
-            <Reports />
-        </Box>
-    );
+  return (
+    <>
+      <Box display="flex" flexDirection="column" justifyContent="center">
+        <Banner source={employerData.backgroundImageURL} />
+        <Stats
+          logo={employerData.imageURL}
+          name={employerData.name}
+          employees={employerData.numEmployees}
+          reports={employerData.numReports}
+          trend={84}
+          mcr="Racism"
+        />
+        <InstantSearch indexName="Reports" searchClient={searchClient}>
+          <Box
+            display="grid"
+            mt="50px"
+            py="50px"
+            px="100px"
+            gridTemplateColumns="repeat(auto-fit, minmax(400px, 600px))"
+            gridColumnGap="40px"
+            gridRowGap={5}
+            maxWidth="100%"
+            justifyContent="center"
+          >
+            <EmployerHits />
+          </Box>
+        </InstantSearch>
+      </Box>
+    </>
+  );
 };
 
 export default Company;
