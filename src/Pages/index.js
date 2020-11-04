e5abc97e8dbb19a6f7707c89f5d10a307a34b032
Ryan Li <ryan.lichangrui@gmail.com>
2020-11-04 08:31:41 -0500

    fix browse company columns

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -137,7 +137,7 @@ const Index = () => (
                     <Box display="flex" flexDirection="column">
                         <Box
                             display="grid"
-                            gridTemplateColumns="repeat(auto-fit, minmax(320px, 1fr))"
+                            gridTemplateColumns={["1fr", null, "1fr 1fr", null]}
                             gridColumnGap="40px"
                             gridRowGap="40px"
                             maxWidth="100%"
@@ -145,7 +145,7 @@ const Index = () => (
                         >
                             <EmployerHits />
                         </Box>
-                        <Box marginTop="auto">
+                        <Box marginTop="20px">
                             <Pagination showFirst={false} />
                         </Box>
                     </Box>
